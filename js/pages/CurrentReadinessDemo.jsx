c62b47c895262709bbf4457eed16ca8e572ca70f
Denis McPhillips <mcphillipsd@nextcapital.com>
2025-10-15 15:26:03 -0400

    Update Current Readiness Static demo to show compact and non-compact styles

--- js/pages/CurrentReadinessDemo.jsx
+++ js/pages/CurrentReadinessDemo.jsx
@@ -5,22 +5,34 @@ import { NextCapitalComponent } from '@nextcapital/client';
 import SolutionSpecificDemo from '../components/SolutionSpecificDemo';
 
 import Page from '../components/Page';
+import SimpleSpacer from '../components/SimpleSpacer';
 import WidgetBox from '../components/WidgetBox';
 
-const CurrentReadinessWidget = () => (
-  <WidgetBox width="medium" height="auto">
-    <NextCapitalComponent
-      getEmbed={
-        (client) => new client.CurrentReadiness({
-          onNavigateToFullExperience: () => window.alert('personalize button clicked!'),
-          onNavigateToInteractive: () => window.alert('do better button clicked!'),
-          onNavigateToSaveMoreNow: () => window.alert('save more now button clicked!'),
-          onError: () => console.error('CurrentReadiness Demo: error occurred!')
-        })
-      }
-      loadingContent="loading..."
-    />
-  </WidgetBox>
+const CurrentReadinessWidget = ({ title, useCompactStyle = false }) => (
+  <div>
+    <h2>{ title }</h2>
+    <WidgetBox width="medium" height="auto">
+      <NextCapitalComponent
+        getEmbed={
+          (client) => new client.CurrentReadiness({
+            useCompactStyle,
+            onNavigateToFullExperience: () => window.alert('personalize button clicked!'),
+            onNavigateToInteractive: () => window.alert('do better button clicked!'),
+            onNavigateToSaveMoreNow: () => window.alert('save more now button clicked!'),
+            onError: () => console.error('CurrentReadiness Demo: error occurred!')
+          })
+        }
+        loadingContent="loading..."
+      />
+    </WidgetBox>
+  </div>
+);
+
+const AllCurrentReadinessWidgetStyles = () => (
+  <SimpleSpacer>
+    <CurrentReadinessWidget title="Non-Compact Style" />
+    <CurrentReadinessWidget title="Compact Style" useCompactStyle />
+  </SimpleSpacer>
 );
 
 /**
@@ -31,7 +43,7 @@ const CurrentReadinessWidget = () => (
 const CurrentReadinessDemo = () => (
   <Page fullScreen>
     <SolutionSpecificDemo
-      getChildren={ () => <CurrentReadinessWidget /> }
+      getChildren={ () => <AllCurrentReadinessWidgetStyles /> }
       module="CurrentReadiness"
     />
   </Page>
